6ecd15bd5fd6720c01f648dd36042dcd11f9e004
Chase Schwalbach <chase.schwalbach@tempus.com>
2019-08-01 14:27:41 -0500

    checking name

--- usewasteless/usewasteless_app/models.py
+++ usewasteless/usewasteless_app/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.contrib.postgres.fields import JSONField
 from django.conf import settings
 
+"""
+Need to figure out cost. How do I want to do it?
+
+# Food Items
+# Synonyms
+# Replacements
+# Cost table?
+"""
+
 
 class FoodItems(models.Model):
     food_item_name = models.CharField(max_length=200)
@@ -28,6 +37,7 @@ class Recipes(models.Model):
 class Pantries(models.Model):
     food_item_id = models.ManyToManyField(FoodItems)
     cost_cents_per_unit = models.IntegerField(default=0)
+    cost_cents = models.IntegerField(default=0)
     best_by_date = models.DateTimeField('best by date')
     amount_bought = models.FloatField(default=0)
     amount_left = models.FloatField(default=0)
@@ -48,7 +58,17 @@ class Synonyms(models.Model):
 
 class Replacements(models.Model):
     food_item_id = models.ManyToManyField(FoodItems)
-    replacement_food_item_ids = models.ManyToManyField(FoodItems)
+    replacement_food_item_ids = models.ManyToManyField(FoodItems, related_name="replacement_ids")
     replacement_steps = JSONField()
+    added_date = models.DateTimeField('date added', auto_now_add=True)
+    updated_date = models.DateTimeField('date last updated', auto_now=True)
+
+
+class FoodCosts(models.Model):
+    food_item_id = models.ManyToManyField(FoodItems)
+    pantry_added_to = models.ManyToManyField(Pantries)
+    cost_cents_per_unit = models.IntegerField(default=0)
+    cost_cents = models.IntegerField(default=0)
+    amount_bought = models.FloatField(default=0)
     added_date = models.DateTimeField('date added', auto_now_add=True)
     updated_date = models.DateTimeField('date last updated', auto_now=True)
